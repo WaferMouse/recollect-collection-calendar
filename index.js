5d79c4dbb2d7451f3fdb368d5ec387bee0e638aa
Jocelyne Jones <gothling@jocely.net>
2026-01-06 00:09:25 +0000

    color-coded emojis

--- index.js
+++ index.js
@@ -5,6 +5,93 @@ var https = require('https');
 var utcTwoDaysAgo = new Date(Date.now() - (86400000 * 2)).toJSON().slice(0,10);
 var utcOneYearFromNow = new Date(Date.now() + (86400000 * 365)).toJSON().slice(0,10);
 
+const COLORS = [
+    {
+        'name': 'red',
+        'rgb': 0xe00000,
+        'emoji': '🟥'
+    },
+    {
+        'name': 'orange',   
+        'rgb': 0xc06000,
+        'emoji': '🟧'
+    },
+    {
+        'name': 'yellow',
+        'rgb': 0xe0c000,
+        'emoji': '🟨'
+    },
+    {
+        'name': 'green',
+        'rgb': 0x00a000,
+        'emoji': '🟩'
+    },
+    {
+        'name': 'blue',
+        'rgb': 0x0080e0,
+        'emoji': '🟦'
+    },
+    {
+        'name': 'purple',
+        'rgb': 0x804000,
+        'emoji': '🟪'
+    },
+    {
+        'name': 'brown',
+        'rgb': 0xa080c0,
+        'emoji': '🟫'
+    },
+    {
+        'name': 'black',
+        'rgb': 0x000000,
+        'emoji': '⬛'
+    },
+    {
+        'name': 'white',
+        'rgb': 0xffffff,
+        'emoji': '⬜'
+    },
+]
+
+var colorMap = [];
+
+function toColor(num) {
+    num >>>= 0;
+    var b = num & 0xFF,
+        g = (num & 0xFF00) >>> 8,
+        r = (num & 0xFF0000) >>> 16
+    return ({'r': r, 'g': g, 'b': b});
+}
+
+function findRgbDistance(a, b) {
+    return (
+        Math.sqrt((
+            Math.pow(a.r - b.r, 2) +
+            Math.pow(a.g - b.g, 2) +
+            Math.pow(a.b - b.b, 2)
+        ))
+    )
+}
+
+function findPaletteEntry(rgb) {
+    var shortest_distance = 0xffffff;
+    var best_candidate = COLORS[0];
+    COLORS.forEach(
+        (el) => {
+            var distance = findRgbDistance(toColor(el.rgb), rgb);
+            if (distance < shortest_distance) {
+                shortest_distance = distance;
+                best_candidate = el;
+            }
+        }
+    )
+    return(best_candidate);
+}
+
+for (let i = 0; i < 0x1000000; i++) {
+    colorMap.push(findPaletteEntry(toColor(i))['emoji']);
+}
+
 app.get("/:place",async function(request,response){
     response.type('text/calendar');
     var getURL = "https://api.eu.recollect.net/api/places/" + request.params.place + "/services/50003/events?hide=reminder_only&nomerge=true&locale=en-GB&after=" + utcTwoDaysAgo + "&before=" + utcOneYearFromNow;
@@ -37,12 +124,13 @@ function veventMaker(event_in){
     var subject = flags['subject'];
     var uid = dtStart + name + "@recollect.jocely.net";
     var desc = flags['html_message'].replace(/(<([^>]+)>)/ig, '');
+    var color = colorMap[parseInt(flags['color'].slice(1), 16)];
     return(
 `BEGIN:VEVENT
 UID:` + uid + `
 DTSTAMP:` + stamp + `
 DTSTART:` + dtStart + `
-SUMMARY:` + subject + `
+SUMMARY:` + color + ` ` + subject + `
 DESCRIPTION:` + desc + `
 END:VEVENT`
     )
